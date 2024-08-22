e70c0bdba727dafc9efa327f43846dd04404d604
Nadeem Parokkot <nadeemparokkot@gmail.com>
2024-08-22 11:20:25 +0530

    now we can see backend json at frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 function App() {
 
+  const [user,setUser] = useState([])
+
   const getUser=()=>{
     fetch("/api/user")
     .then(res => res.json())
-    .then(json => console.log(json))
+    .then(json => setUser(json))//console.log(json))
   }
 
 
@@ -17,7 +19,15 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Helloo...........</h1>
+      {user.map((data)=>{
+        return <>
+        <div>
+        <h1>Name : {data.name}</h1>
+        <h1>UserName : {data.username}</h1>
+        <h1>Email : {data.email}</h1>
+        </div>
+        </>
+      })}
     </div>
   );
 }
